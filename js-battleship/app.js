c15cd3fe00db82435ffb046e036f17b7c0cd0403
juliendy <julien.yasar@gmail.com>
2023-02-03 07:20:56 +0100

    chore: flip functionality done / game board appended.

--- js-battleship/app.js
+++ js-battleship/app.js
@@ -1,8 +1,27 @@
+const gamesBoardContainer = document.querySelector("#gamesboard-container");
 const optionContainer = document.querySelector(".option-container");
 const flipButton = document.querySelector("#flip-button");
 
+// Option choosing
+let angle = 0;
 function flip() {
-    console.log("options: ", optionContainer.children);
+    const optionShips = Array.from(optionContainer.children);
+    angle = angle === 0 ? 90 : 0;
+    optionShips.forEach(
+        (optionShips) => (optionShips.style.transform = `rotate(${angle}deg)`)
+    );
 }
 
+// Creating Boards
+const width = 10;
+
+function createBoard() {
+    const gameBoardContainer = document.createElement("div");
+    gameBoardContainer.classList.add("game-board");
+    gameBoardContainer.style.backgroundColor = "pink";
+
+    gamesBoardContainer.append(gameBoardContainer);
+}
+
+createBoard()
 flipButton.addEventListener("click", flip);
