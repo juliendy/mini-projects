56e6ecf734eba16424a6edb968ff76d42b9433e4
juliendy <julien.yasar@gmail.com>
2023-02-03 10:25:02 +0100

    chore: block piece logic edgecases fixed(?)

--- js-battleship/app.js
+++ js-battleship/app.js
@@ -20,54 +20,84 @@ function createBoard(color, user) {
     const gameBoardContainer = document.createElement("div");
     gameBoardContainer.classList.add("game-board");
     gameBoardContainer.style.backgroundColor = color;
-    gameBoardContainer.id = user
+    gameBoardContainer.id = user;
 
     for (let i = 0; i < width * width; i++) {
-        const block = document.createElement('div')
-        block.classList.add('block')
-        block.id = i
-        gameBoardContainer.append(block)
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.id = i;
+        gameBoardContainer.append(block);
     }
     gamesBoardContainer.append(gameBoardContainer);
 }
 
-createBoard('yellow', 'player')
-createBoard('pink', 'computer')
+createBoard("yellow", "player");
+createBoard("pink", "computer");
 
 // Creating Ships
 class Ship {
     constructor(name, length) {
-        this.name = name
-        this.length = length
+        this.name = name;
+        this.length = length;
     }
 }
 
-const destroyer = new Ship('destroyer', 2)
-const submarine = new Ship('submarine', 3)
-const cruiser = new Ship('cruiser', 3)
-const battleship = new Ship('battleship', 4)
-const carrier = new Ship('carrier', 5)
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carrier", 5);
 
-const ships = [destroyer, submarine, cruiser, battleship, carrier]
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
 
 function addShipPiece(ship) {
-    const allBoardBlocks = document.querySelectorAll('#computer div')
-    let randomBoolean = Math.random() < 0.5
+    const allBoardBlocks = document.querySelectorAll("#computer div");
+    let randomBoolean = Math.random() < 0.5;
     let isHorizontal = randomBoolean;
-    let randomStartIndex = Math.floor(Math.random() * width * width)
-    console.log('randomStartIndex: ', randomStartIndex);
+    let randomStartIndex = Math.floor(Math.random() * width * width);
 
-    let shipBlocks = []
+    let validStart = isHorizontal
+        ? randomStartIndex <= width * width - ship.length
+            ? randomStartIndex
+            : width * width - ship.length
+        : // handle vertical
+        randomStartIndex <= width * width - width * ship.length
+        ? randomStartIndex
+        : randomStartIndex - ship.length * width + width;
+
+    let shipBlocks = [];
     for (let i = 0; i < ship.length; i++) {
         if (isHorizontal) {
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex + i)])
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i]);
         } else {
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width])
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i * width]);
         }
     }
-    shipBlocks.forEach(shipBlock => {
-        shipBlock.classList.add(ship.name)
-        shipBlock.classList.add('taken')
-    })
+
+    let valid;
+    if (isHorizontal) {
+        valid = shipBlocks.every(
+            (_shipBlock, index) =>
+                shipBlocks[0].id % width !==
+                width - (shipBlocks.lenght - (index + 1))
+        );
+    } else {
+        valid = shipBlocks.every(
+            (_shipBlock, index) => shipBlocks[0].id < 90 + (width * index + 1)
+        );
+    }
+
+    const notTaken = shipBlocks.every(
+        (shipBlock) => !shipBlock.classList.contains('taken' )
+    );
+
+    if (valid && notTaken) {
+        shipBlocks.forEach((shipBlock) => {
+            shipBlock.classList.add(ship.name);
+            shipBlock.classList.add("taken");
+        });
+    } else {
+        addShipPiece(ship);
+    }
 }
-ships.forEach(ship => addShipPiece(ship))
+ships.forEach((ship) => addShipPiece(ship));
