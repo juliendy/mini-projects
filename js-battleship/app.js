d87a83a96a1bb583281355e5ad11e3fb3a610841
juliendy <julien.yasar@gmail.com>
2023-02-03 12:19:19 +0100

    chore: initiating drag capabilities.

--- js-battleship/app.js
+++ js-battleship/app.js
@@ -101,3 +101,11 @@ function addShipPiece(ship) {
     }
 }
 ships.forEach((ship) => addShipPiece(ship));
+
+// Drag player ships
+const optionShips = Array.from(optionContainer.children)
+optionShips.forEach(optionShip => optionShip.addEventListener('dragstart', dragStart))
+
+function dragStart(e) {
+    console.log(e.target)
+}
