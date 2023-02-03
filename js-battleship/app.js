38a8781dff74b9895d3530bad490bd545addfa70
juliendy <julien.yasar@gmail.com>
2023-02-03 08:42:58 +0100

    chore: board done, grid boxes in place.

--- js-battleship/app.js
+++ js-battleship/app.js
@@ -11,17 +11,25 @@ function flip() {
         (optionShips) => (optionShips.style.transform = `rotate(${angle}deg)`)
     );
 }
+flipButton.addEventListener("click", flip);
 
 // Creating Boards
 const width = 10;
 
-function createBoard() {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement("div");
     gameBoardContainer.classList.add("game-board");
-    gameBoardContainer.style.backgroundColor = "pink";
+    gameBoardContainer.style.backgroundColor = color;
+    gameBoardContainer.id = user
 
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
     gamesBoardContainer.append(gameBoardContainer);
 }
 
-createBoard()
-flipButton.addEventListener("click", flip);
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
